95a537e574cc9c852c62da320ec585ecb363beb9
Matteo Gariglio <matteo.gariglio@proton.ch>
2022-09-14 17:19:02 +0200

    Add project files.

---
+++ LlamaGeneratorTools/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Windows.Input;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace RpgFilesGeneratorTools;
+
+public sealed partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AboutCommand = new RelayCommand(ShowAboutDialog);
+    }
+
+    public ICommand AboutCommand { get; }
+
+    private static async void ShowAboutDialog()
+    {
+        var dialog = new ContentDialog
+        {
+            XamlRoot = App.MainRoot.XamlRoot,
+            Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+            Title = "About",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Close,
+            Content = new AboutView()
+        };
+
+        await dialog.ShowAsync();
+    }
+}
